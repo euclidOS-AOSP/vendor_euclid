3c29c838c2bc226eaf0b6e94515f748a0e2db0ce
minaripenguin <minaripenguin@users.noreply.github.com>
2025-09-20 09:04:36 +0000

    config: Load boot image profile from frameworks/base
    [YAAP:] art boot image profile is only intended for thin art builds
    without frameworks/base.
    
    Change-Id: I60f3117c6ba948311af9f3fe048d0cf304fc283a
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -271,6 +271,9 @@ CUSTOM_LOCALES += \
     cy_GB \
     fur_IT
 
+# Use a generic profile based boot image by default
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 include vendor/euclid/config/version.mk
 include vendor/euclid/config/euclid.mk
