9abbdad5f348ecf5eb098164f321847cc3d34f1b
ᴛʀɪᴅᴇᴠ | トリデブ <tridev09@zohomail.in>
2026-01-07 23:34:52 +0530

    Enhance build output with duration and details
    Added build duration and completion messages to the build process.

--- build/tasks/euclid.mk
+++ build/tasks/euclid.mk
@@ -20,11 +20,29 @@ EUCLID_TARGET_PACKAGE := $(PRODUCT_OUT)/euclidOS-$(EUCLID_BUILD_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+BUILD_START := $(shell date +%s)
 $(EUCLID_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(EUCLID_TARGET_PACKAGE)
-	$(hide) $(SHA256) $(EUCLID_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(EUCLID_TARGET_PACKAGE).sha256sum
-	$(hide) ./vendor/euclid/build/tasks/ascii_output.sh
-	@echo "Package Complete: $(EUCLID_TARGET_PACKAGE)" >&2
+	$(hide) $(SHA256) $(EUCLID_TARGET_PACKAGE) > $(EUCLID_TARGET_PACKAGE).sha256sum
+	$(hide) BUILD_END=$$(date +%s); \
+	BUILD_DIFF=$$((BUILD_END - $(BUILD_START))); \
+	echo -e ${CL_BLD}${CL_GRN}"==========================================================="${CL_RST}; \
+	echo -e ${CL_BLD}${CL_GRN}"            🚀 BUILD COMPLETED SUCCESSFULLY 🚀             "${CL_RST}; \
+	echo -e ${CL_BLD}${CL_GRN}"==========================================================="${CL_RST}; \
+	./vendor/euclid/build/tasks/ascii_output.sh; \
+	echo -e ${CL_BLD}${CL_RED}"🏷️   Version       : "${CL_GRN} $(EUCLID_VERSION)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"📱  Device         : "${CL_GRN} $(PRODUCT_DEVICE)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"👨‍💻  Maintainer     : "${CL_GRN} $(EUCLID_MAINTAINER)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"⏳   Build Start    : "$(shell date -d @$(BUILD_START))${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"✅   Build End      : "$(shell date -d @$$BUILD_END)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_YLW}"⏱️   Duration       : $$(printf "%02d:%02d:%02d" $$((BUILD_DIFF/3600)) $$(((BUILD_DIFF/60)%60)) $$((BUILD_DIFF%60)))"${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"💾  Package Size   : "${CL_GRN} $$(du -h $(EUCLID_TARGET_PACKAGE) | cut -f1)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"📦  ROM PATH       : "${CL_GRN} $(EUCLID_TARGET_PACKAGE)${CL_RST}; \
+	echo -e ${CL_BLD}${CL_RED}"🔒  SHA256 File    : "${CL_GRN} $(EUCLID_TARGET_PACKAGE).sha256sum${CL_RST}; \
+	echo -e ${CL_BLD}${CL_GRN}"==========================================================="${CL_RST}; \
+	echo -e ${CL_BLD}${CL_GRN}"🎉           Thanks for building EuclidOS-AOSP           ❤️"${CL_RST}; \
+	echo -e ${CL_BLD}${CL_GRN}"==========================================================="${CL_RST}; \
+	echo ""
 
 .PHONY: euclid
-euclid: $(EUCLID_TARGET_PACKAGE) $(DEFAULT_GOAL)
+euclid: $(EUCLID_TARGET_PACKAGE)
