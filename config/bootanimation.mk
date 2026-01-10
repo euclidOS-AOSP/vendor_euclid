a3053c178e074afb399d0ebd7c13e3421257b767
TRIDEV099 <tridev09@zohomail.in>
2026-01-10 20:31:17 +0530

    bootanimation: add configurable resolution support
    Previously bootanimation was fixed to 1080p.
    Now TARGET_BOOT_ANIMATION_RES allows choosing 720p or 1080p at build time,
    with safe fallback to 1080p if undefined or unsupported.
    
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,2 +1,17 @@
 # Bootanimation
-    PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning ": Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/euclid/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif
